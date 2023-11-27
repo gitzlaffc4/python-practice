cc5f39e8e8c437d298f08d05e1757c2754dca4ab
camerongitzlaff <gitzlaffc4@gmail.com>
2023-11-27 14:46:54 -0600

    completed excercise 9

---
+++ Excercises/1_Character_Input.py
@@ -0,0 +1,14 @@
+#https://www.practicepython.org/exercise/2014/01/29/01-character-input.html
+def excercise_1():
+    name = input("What is your name? : ")
+    age = int(input("How old are you? : "))
+    how_many = int(input("How many times should I introduce you? :"))
+    turn_100 = 2023 + (100 - age)
+
+    n = 0
+    while n < how_many:
+        print("Hello " + name + ", you will turn 100 years old in " + str(turn_100) + ".")
+        print("")
+        n += 1
+    excercise_1()
+excercise_1()
---
+++ Excercises/3_List_Less_Than_Ten.py
@@ -0,0 +1,12 @@
+#https://www.practicepython.org/exercise/2014/02/15/03-list-less-than-ten.html
+def excercise_3():
+    a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    b = []
+    smaller = int(input("Find numbers less than : "))
+
+    for element in a:
+        if element < smaller:
+            b.append(element)
+    print(b)
+
+excercise_3()
